bf15cd18d73fd9c38cccd3eb05ef5f68e88d593b
Ayush <sayhi.ayush@gmail.com>
2025-05-26 11:43:51 +0530

    images aligned

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -1,19 +1,26 @@
-import React, { useContext } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import {Link} from 'react-router-dom'
+import React, { useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import { Link } from 'react-router-dom';
 
-const ProductItem = ({id, image, name, price}) => {
+const ProductItem = ({ id, image, name, price }) => {
+  const { currency } = useContext(ShopContext);
 
-    const {currency} = useContext(ShopContext);
   return (
-    <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
-        <div className='overflow-hidden'>
-            <img src={image[0]} alt="" className='hover:scale-110 transition ease-in-out'/>
-        </div>
-        <p className='pt-3 pb-1 text-sm'>{name}</p>
-        <p className='text-sm font-medium'>{currency}{price}</p>
+    <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
+      <div className="w-full h-64 bg-gray-100 rounded-lg overflow-hidden">
+        <img
+          src={image[0]}
+          alt={name}
+          className="w-full h-full object-fill hover:scale-110 transition-transform duration-300 ease-in-out"
+        />
+      </div>
+      <p className="pt-3 pb-1 text-sm line-clamp-1">{name}</p>
+      <p className="text-sm font-medium">
+        {currency}
+        {price}
+      </p>
     </Link>
-  )
-}
+  );
+};
 
-export default ProductItem
+export default ProductItem;
